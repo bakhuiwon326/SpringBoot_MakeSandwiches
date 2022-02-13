905a1b86393a1267414b3a2b8b6ecc2f3ffbd626
bakhuiwon326 <79272597+bakhuiwon326@users.noreply.github.com>
2022-02-13 23:14:23 +0900

    프로젝트 생성

---
+++ sandwichesMaker/src/main/java/com/sandwiches/programming/sandwichesMaker/SandwichesMakerApplication.java
@@ -0,0 +1,13 @@
+package com.sandwiches.programming.sandwichesMaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandwichesMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandwichesMakerApplication.class, args);
+	}
+
+}
---
+++ sandwichesMaker/src/test/java/com/sandwiches/programming/sandwichesMaker/SandwichesMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandwiches.programming.sandwichesMaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandwichesMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
